7040f7f5999d9a8d226e47c287fa7a195d7d9277
amir1611 <amirjr1611@gmail.com>
2024-11-15 11:49:01 +0800

    first commit

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'address' => $this->faker->address,
+            'phoneNumber' => $this->faker->phoneNumber,
+            'gender' => $this->faker->randomElement(['Male', 'Female']),
+            'birthday' => $this->faker->date(),
+        ];
+    }
+}
